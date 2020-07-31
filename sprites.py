826a446823f1661c3a3f7e6c75c78038ffb77c82
bearheathen <brandondstewart@gmail.com>
2020-07-31 10:46:49 -0500

    I don't even remember

--- sprites.py
+++ sprites.py
@@ -70,3 +70,8 @@ class Wall(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x * TILE_SIZE
         self.rect.y = y * TILE_SIZE
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
